70b242f0e3fc6455a991f4b8f7eae675fff73fef
Sven Mengler <s.s.mengler@gmx.net>
2017-11-03 11:48:52 +0100

    finished init function

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,9 +1,20 @@
+import numpy as np
+
 # neural network class
 class neuralNetwork:
 
-	# initialise
-	def __init__():
+	# initialise neural network with its parameters
+	def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
 
+		# initialise sizes
+		self.iNodes = inputNodes
+		self.hNodes = hiddenNodes
+		self.oNodes = outputNodes
+		self.lr = learningRate
+
+		# make weight matrices
+		self.Wih = np.random.normal(0.0, pow(self.hNodes, -0.5), (self.hNodes, self.iNodes))
+		self.Who = np.random.normal(0.0, pow(self,oNodes, -0.5), (self.oNodes, self.hNodes))
 		pass
 
 	# train
