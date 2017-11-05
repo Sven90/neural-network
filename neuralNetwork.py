f0b59c2c77dc45ba2ba4bae5cc7ec9fdb534f7de
Sven Mengler <s.s.mengler@gmx.net>
2017-11-05 18:05:14 +0100

    improved code readability

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,5 +1,7 @@
+# imports
 import numpy as np
-import scipy.special
+import scipy.special as spec
+import matplotlib.pyplot as plt
 
 # neural network class
 class neuralNetwork:
@@ -8,70 +10,63 @@ class neuralNetwork:
 	def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
 
 		# initialise sizes
-		self.iNodes = inputNodes
-		self.hNodes = hiddenNodes
-		self.oNodes = outputNodes
-		self.lr = learningRate
+		self.inputNodes = inputNodes
+		self.hiddenNodes = hiddenNodes
+		self.outputNodes = outputNodes
+		self.learningRate = learningRate
 
 		# make weight matrices
-		self.Wih = np.random.normal(0.0, pow(self.hNodes, -0.5), (self.hNodes, self.iNodes))
-		self.Who = np.random.normal(0.0, pow(self.oNodes, -0.5), (self.oNodes, self.hNodes))
+		self.WeightsInputHidden = np.random.normal(0.0, pow(self.hiddenNodes, -0.5), (self.hiddenNodes, self.inputNodes))
+		self.WeightsHiddenOutput = np.random.normal(0.0, pow(self.outputNodes, -0.5), (self.outputNodes, self.hiddenNodes))
 		
 		# define activation function
-		self.activationFunction = lambda x: scipy.special.expit(x)
+		self.activationFunction = lambda x: spec.expit(x)
 		
 		pass
 
-	# query the neural network query([1, 3, 4, 5, ...])
+	# query the neural network like query([1, 3, 4, 5, ...])
 	def query(self, inputList):
 
 		# convert input list to vector
-		inp = np.array(inputList, ndmin=2).T
+		inputVector = np.array(inputList, ndmin=2).T
 
 		# calc first cycle
-		hInp = np.dot(self.Wih, inp)
-		hOut = self.activationFunction(hInp)
+		hiddenInputVector = np.dot(self.WeightsInputHidden, inputVector)
+		hiddenOutputVector = self.activationFunction(hiddenInputVector)
 
 		# calc final cycle
-		fInp = np.dot(self.Who, hOut)
-		fOut = self.activationFunction(fInp)
+		finalInputVector = np.dot(self.WeightsHiddenOutput, hiddenOutputVector)
+		finalOutputVector = self.activationFunction(finalInputVector)
 
-		return fOut
-
-		pass
+		return finalOutputVector
 
 	# train the neural network
 	def train(self, inputList, targetList):
 
 		# calc output like query function
-		inp = np.array(inputList, ndmin=2).T
-		hInp = np.dot(self.Wih, inp)
-		hOut = self.activationFunction(hInp)
-		fInp = np.dot(self.Who, hOut)
-		fOut = self.activationFunction(fInp)
+		inputVector = np.array(inputList, ndmin=2).T
+		hiddenInputVector = np.dot(self.WeightsInputHidden, inputVector)
+		hiddenOutputVector = self.activationFunction(hiddenInputVector)
+		finalInputVector = np.dot(self.WeightsHiddenOutput, hiddenOutputVector)
+		finalOutputVector = self.activationFunction(finalInputVector)
 
 		# calc final error
 		target = np.array(targetList, ndmin=2).T
-		fErr = target - fOut
+		finalError = target - finalOutputVector
 
 		# calc hidden error
-		hErr = np.dot(self.Who.T, fErr)
+		hiddenError = np.dot(self.WeightsHiddenOutput.T, finalError)
 
-		# update ho layer
-		self.Who += self.lr * np.dot((fErr * fOut * (1.0 - fOut)), np.transpose(hOut))
+		# update final layer
+		self.WeightsHiddenOutput += self.learningRate * np.dot((finalError * finalOutputVector * (1.0 - finalOutputVector)), np.transpose(hiddenOutputVector))
 
-		# update ih layer
-		self.Wih += self.lr * np.dot((hErr * hOut * (1.0 - hOut)), np.transpose(inp))
+		# update first layer
+		self.WeightsInputHidden += self.learningRate * np.dot((hiddenError * hiddenOutputVector * (1.0 - hiddenOutputVector)), np.transpose(inputVector))
 
 		pass
 
 	pass
 
-# test: handwritten numbers
-# open file
-dataFile = open("mnist_train.csv", 'r')
-dataList = dataFile.readlines()
-dataFile.close()
+# train neural network for handwritten number recognition
 
-print(len(dataList))
-print(dataList[0])
+
